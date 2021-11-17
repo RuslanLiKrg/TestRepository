d84c1269dc3c9cff53e9292f364af47e05432e9d
Ruslan <ruslan022015@gmail.com>
2021-11-17 21:42:57 +0600

    added geometry class

---
+++ SumApp/Geometry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SumApp
+{
+    class Geometry
+    {
+        public decimal lenght { get; set; }
+        public decimal wight { get; set; }
+        public decimal GetSquare()
+        {
+            throw new NotSupportedException();
+        }
+        public decimal GetPerimetr()
+        {
+            throw new NotSupportedException();
+        }
+        public decimal Getwight(decimal square)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
